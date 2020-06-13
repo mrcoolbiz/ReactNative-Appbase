365eea15f4d2857f55baa6708514b8914c517616
Tuan Nguyen <mrcoolbiz@gmail.com>
2020-06-13 11:48:29 -0500

    Install: - React Native - React Native Nagivation - Setup Basic Nagivation & HomeScreen.

---
+++ screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default class HomeScreen extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Home Screen</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
